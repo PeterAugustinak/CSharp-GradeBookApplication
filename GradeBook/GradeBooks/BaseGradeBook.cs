9bd0994e119c8aabfc6a5d141d7636160e7fe4e9
PeterAugustinak <48356585+PeterAugustinak@users.noreply.github.com>
2019-09-02 16:29:28 +0200

    07 Add Support for Weighted Grading
    - create isWeighted Propert
    - refactor GradeBooks and StartingUserInterface
    - set isWeighted in the BaseGradeBook Constructor
    - update StartinUserInterface's CreateCommand Method's condition
    - Update BaseGradeBook's GetGPA Method
    - Update StartingUserInterface HelpCommand Method

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -14,10 +14,12 @@ namespace GradeBook.GradeBooks
         public string Name { get; set; }
         public List<Student> Students { get; set; }
         public GradeBookType Type { get; set;  }
+        public bool IsWeighted { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
+            IsWeighted = isWeighted;
             Students = new List<Student>();
         }
 
@@ -107,18 +109,23 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            int specialAddition = 0;
+
+            if (IsWeighted && (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled))
+                specialAddition = 1;
+
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    return 4 + specialAddition;
                 case 'B':
-                    return 3;
+                    return 3 + specialAddition;
                 case 'C':
-                    return 2;
+                    return 2 + specialAddition;
                 case 'D':
-                    return 1;
+                    return 1 + specialAddition;
                 case 'F':
-                    return 0;
+                    return 0 + specialAddition;
             }
             return 0;
         }
